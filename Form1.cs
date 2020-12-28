308fc26f86b0590997ece6ea133417a47dd91dd8
warckenergy <warckenergy@gmail.com>
2020-12-28 03:36:59 +0300

    Variable.1

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Kr_DB
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["Personnel"].ConnectionString);
+            sqlConnection.Open();
+            //if (sqlConnection.State == ConnectionState.Open)
+            //{
+            //    MessageBox.Show("Подключение установлено!");
+            //}
+        }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Personnel]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Patronymic] NVARCHAR(50) NOT NULL, 
+    [Surname] NVARCHAR(50) NOT NULL, 
+    [Gender] NVARCHAR(50) NOT NULL, 
+    [Birthday] DATE NOT NULL, 
+    [Place of birth] NVARCHAR(50) NOT NULL, 
+    [Phone] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL
+)
